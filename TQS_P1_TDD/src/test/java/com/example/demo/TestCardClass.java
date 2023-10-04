727742cd2117de328abcd4ffe7c6a3fbeb777d7c
TheRospetit <janrubiorico@gmail.com>
2023-10-04 20:18:44 +0200

    TestCardClass
    First CardClass Test Fail

---
+++ TQS_P1_TDD/src/test/java/com/example/demo/TestCardClass.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCardClass {
+    CardClass cartita;
+    @Test
+    public void TestConstructor(){
+        cartita = new CardClass();
+    }
+}
