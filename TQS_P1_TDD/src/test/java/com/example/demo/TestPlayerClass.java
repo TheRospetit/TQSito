c47f44a72aafa9d1dfa511e19a4d879c88dfef59
TheRospetit <janrubiorico@gmail.com>
2023-10-16 19:25:17 +0200

    Test player creation

---
+++ TQS_P1_TDD/src/test/java/com/example/demo/TestPlayerClass.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class TestPlayerClass {
+
+    Player player;
+    @Test
+    public void TestConstructor(){
+        player = new Player();
+    }
+}
